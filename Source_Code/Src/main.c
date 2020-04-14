f66caf76d890f3844f451cede20e773182f7d729
jaimdu <63657228+jaimdu@users.noreply.github.com>
2020-04-14 20:55:32 +0530

    LED Blink Implemented
    GPIOA, Black board LED blink program added.

--- Source_Code/Src/main.c
+++ Source_Code/Src/main.c
@@ -95,9 +95,12 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	  	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+	  	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+	  	  HAL_Delay(1000);
+	  	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+	  	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET);
+	  	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
